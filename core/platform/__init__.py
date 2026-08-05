45636d686c0dc73f5d193ad0205b2d590a00f9e7
qihang518887 <876849639@qq.com>
2026-08-05 15:24:26 +0800

    chore: reorder platform imports

--- core/platform/__init__.py
+++ core/platform/__init__.py
@@ -1,7 +1,7 @@
 from .base import BaseMusicPlayer
-from .kg import KuGouMusic
-from .kw import KuwoMusic
 from .ncm import NetEaseMusic
 from .tx import QQMusic
+from .kg import KuGouMusic
+from .kw import KuwoMusic
 
 __all__ = ["NetEaseMusic", "QQMusic", "KuGouMusic", "KuwoMusic", "BaseMusicPlayer"]
